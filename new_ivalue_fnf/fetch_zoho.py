d97c4d5612639cd47910ad9ad47cd70bbba11817
KhaledMJallad <Khaled.jalad@ivalueconsult.com>
2026-05-21 10:59:55 +0300

    update cron job

--- new_ivalue_fnf/fetch_zoho.py
+++ new_ivalue_fnf/fetch_zoho.py
@@ -6,7 +6,8 @@ def fetch_zoho_doc():
     get_all_sigend_fnf  = frappe.db.sql(''' 
                                                 SELECT 
                                                     fnf.`name`,
-                                                    fnf.zoho_id
+                                                    fnf.zoho_id,
+                                                    fnf.docstatus
                                                 FROM `tabFull and Final Statement` AS fnf
                                                 LEFT JOIN `tabCustody Status` AS cs
                                                     ON cs.parent = fnf.name
@@ -14,14 +15,13 @@ def fetch_zoho_doc():
                                                 ON f.attached_to_name = fnf.name 
                                                 WHERE 
                                                     fnf.`zoho_id` IS NOT NULL
-                                                    AND fnf.`docstatus` = 1
                                                     AND f.name is NULL
                                             ''', (), as_dict=True)
     
     
     if get_all_sigend_fnf:
         for custody in get_all_sigend_fnf:
-            zoho = Zoho_api(doctype = 'Full and Final Statement', name = custody.name, status_child_table_name = "Custody Status", zoho_id = custody.zoho_id)
+            zoho = Zoho_api(doctype = 'Full and Final Statement', name = custody.name, status_child_table_name = "Custody Status", zoho_id = custody.zoho_id, docstatus= custody.docstatus)
             zoho_status = zoho.fetch_zoho_status()
             update_workfow_status(custody.name ,zoho_status['zoho_status'])
 
@@ -31,6 +31,7 @@ def update_workfow_status(name, zoho_status):
     
     if zoho_status == "completed":
         frappe.db.set_value('Full and Final Statement', name, 'workflow_state', 'Signed')
+        frappe.db.set_value("Full and Final Statement", doc.name, "docstatus", 1)
         frappe.db.commit()
         return "completed"
     elif zoho_status == "declined":
